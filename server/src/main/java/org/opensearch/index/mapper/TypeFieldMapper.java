5082df9a1bf18be51743eaed472f28a4cf81ca6b
Suraj Singh <surajrider@gmail.com>
2022-03-24 17:30:06 -0700

    Remove type handling in LeafFieldsLookup
    Signed-off-by: Suraj Singh <surajrider@gmail.com>

--- server/src/main/java/org/opensearch/index/mapper/TypeFieldMapper.java
+++ server/src/main/java/org/opensearch/index/mapper/TypeFieldMapper.java
@@ -68,7 +68,6 @@ public class TypeFieldMapper extends MetadataFieldMapper {
 
     public static final String NAME = "_type";
 
-
     public static final String CONTENT_TYPE = "_type";
 
     public static final TypeParser PARSER = new FixedTypeParser(c -> new TypeFieldMapper());

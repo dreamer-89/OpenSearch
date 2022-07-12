bbd8db755b8a9733b2074198a47bd6e23f40213e
Suraj Singh <surajrider@gmail.com>
2022-07-12 12:25:02 -0700

    Add docRep enabled & Issue 3832 tests
    Signed-off-by: Suraj Singh <surajrider@gmail.com>

--- server/src/internalClusterTest/java/org/opensearch/snapshots/SegmentReplicationSnapshotIT.java
+++ server/src/internalClusterTest/java/org/opensearch/snapshots/SegmentReplicationSnapshotIT.java
@@ -18,6 +18,7 @@ import org.opensearch.action.admin.indices.settings.get.GetSettingsResponse;
 import org.opensearch.action.search.SearchResponse;
 import org.opensearch.cluster.metadata.IndexMetadata;
 import org.opensearch.common.settings.Settings;
+import org.opensearch.common.xcontent.XContentType;
 import org.opensearch.index.query.QueryBuilders;
 import org.opensearch.indices.replication.common.ReplicationType;
 import org.opensearch.rest.RestStatus;
@@ -35,7 +36,7 @@ public class SegmentReplicationSnapshotIT extends AbstractSnapshotIntegTestCase 
     private static final String RESTORED_INDEX_NAME = INDEX_NAME + "-restored";
     private static final int SHARD_COUNT = 1;
     private static final int REPLICA_COUNT = 1;
-    private static final int DOC_COUNT = 100;
+    private static final int DOC_COUNT = 1010;
 
     private static final String REPOSITORY_NAME = "test-segrep-repo";
     private static final String SNAPSHOT_NAME = "test-segrep-snapshot";
@@ -63,13 +64,7 @@ public class SegmentReplicationSnapshotIT extends AbstractSnapshotIntegTestCase 
         return false;
     }
 
-    public void initSetup(Settings indexSettings) throws Exception {
-        // Create 2 node cluster
-        final String nodeA = internalCluster().startNode();
-        final String nodeB = internalCluster().startNode();
-        createIndex(INDEX_NAME, indexSettings);
-        ensureGreen(INDEX_NAME);
-        // Ingest data
+    public void ingestData(int docCount, String indexName) throws Exception {
         try(
             BackgroundIndexer indexer = new BackgroundIndexer(
                 INDEX_NAME,
@@ -80,10 +75,20 @@ public class SegmentReplicationSnapshotIT extends AbstractSnapshotIntegTestCase 
                 false,
                 random())
         ) {
-            indexer.start(DOC_COUNT);
-            waitForDocs(DOC_COUNT, indexer);
-            refresh(INDEX_NAME);
+            indexer.start(docCount);
+            waitForDocs(docCount, indexer);
+            refresh(indexName);
         }
+    }
+
+    public void initSetup(Settings indexSettings) throws Exception {
+        // Create 2 node cluster
+        final String nodeA = internalCluster().startNode();
+        final String nodeB = internalCluster().startNode();
+        createIndex(INDEX_NAME, indexSettings);
+        ensureGreen(INDEX_NAME);
+        // Ingest data
+        ingestData(DOC_COUNT, INDEX_NAME);
     }
 
     public void createSnapshot() {
@@ -140,6 +145,48 @@ public class SegmentReplicationSnapshotIT extends AbstractSnapshotIntegTestCase 
         assertHitCount(resp, DOC_COUNT);
     }
 
+    public void testRestoreOnSegRep_issue3832() throws Exception {
+        initSetup(addShardSettings(segRepEnableIndexSettings()));
+        // Perform indexing, deletion & refresh in a loop
+        final int numDocs = scaledRandomIntBetween(10000, 100000);
+        int delCount = 0, refreshCount = 0;
+        for (int i = 0; i < numDocs; i++) {
+            client().prepareIndex().setIndex(INDEX_NAME).setId(String.valueOf(i)).setSource("{\"foo\": \"bar\"}", XContentType.JSON).get();
+            if (i%5 == 0 && randomBoolean()) {
+                delCount++;
+                client().prepareDelete(INDEX_NAME, String.valueOf(i));
+            }
+            if (i%10 == 0 && randomBoolean()) {
+                refreshCount++;
+                client().admin().indices().prepareRefresh(INDEX_NAME).get();
+            }
+        }
+        logger.info("Del count {}, refresh count {}", delCount, refreshCount);
+        ensureGreen(INDEX_NAME);
+    }
+
+    public void testRestoreOnSegRepDuringIngestion() throws Exception {
+        initSetup(addShardSettings(segRepEnableIndexSettings()));
+        createSnapshot();
+        // Delete index
+        client().admin().indices().delete(new DeleteIndexRequest(INDEX_NAME));
+        Thread.sleep(5000);
+        assertFalse("index [" + INDEX_NAME + "] should have been deleted", indexExists(INDEX_NAME));
+
+        logger.info("Restore from snapshot");
+        RestoreSnapshotResponse restoreSnapshotResponse = restoreSnapshotWithSettings(null);
+
+        // Assertions
+        assertThat(restoreSnapshotResponse.status(), equalTo(RestStatus.ACCEPTED));
+        logger.info("Ensure cluster is green");
+        ingestData(5000, RESTORED_INDEX_NAME);
+        ensureGreen(RESTORED_INDEX_NAME);
+        GetSettingsResponse settingsResponse = client().admin().indices().getSettings(new GetSettingsRequest().indices(RESTORED_INDEX_NAME)).get();
+        assertEquals(settingsResponse.getSetting(RESTORED_INDEX_NAME, "index.replication.type"), "SEGMENT");
+        SearchResponse resp = client().prepareSearch(RESTORED_INDEX_NAME).setQuery(QueryBuilders.matchAllQuery()).get();
+        assertHitCount(resp, DOC_COUNT);
+    }
+
     public void testSnapshotOnDocRep_RestoreOnSegRep() throws Exception {
         initSetup(addShardSettings(docRepEnableIndexSettings()));
         createSnapshot();
@@ -177,6 +224,27 @@ public class SegmentReplicationSnapshotIT extends AbstractSnapshotIntegTestCase 
         ensureGreen(RESTORED_INDEX_NAME);
         GetSettingsResponse settingsResponse = client().admin().indices().getSettings(new GetSettingsRequest().indices(RESTORED_INDEX_NAME)).get();
         assertEquals(settingsResponse.getSetting(RESTORED_INDEX_NAME, "index.replication.type"), "DOCUMENT");
+        SearchResponse resp = client().prepareSearch(RESTORED_INDEX_NAME).setQuery(QueryBuilders.matchAllQuery()).get();
+        assertHitCount(resp, DOC_COUNT);
+    }
+
+    public void testSnapshotOnDocRep_RestoreOnDocRep() throws Exception {
+        initSetup(addShardSettings(docRepEnableIndexSettings()));
+        createSnapshot();
+        // Delete index
+        client().admin().indices().delete(new DeleteIndexRequest(INDEX_NAME));
+        Thread.sleep(5000);
+
+        logger.info("Restore from snapshot");
+        RestoreSnapshotResponse restoreSnapshotResponse = restoreSnapshotWithSettings(docRepEnableIndexSettings().build());
+
+        // Assertions
+        assertThat(restoreSnapshotResponse.status(), equalTo(RestStatus.ACCEPTED));
+        logger.info("Ensure cluster is green");
+        ensureGreen(RESTORED_INDEX_NAME);
+        GetSettingsResponse settingsResponse = client().admin().indices().getSettings(new GetSettingsRequest().indices(RESTORED_INDEX_NAME)).get();
+        assertEquals(settingsResponse.getSetting(RESTORED_INDEX_NAME, "index.replication.type"), "DOCUMENT");
+
         SearchResponse resp = client().prepareSearch(RESTORED_INDEX_NAME).setQuery(QueryBuilders.matchAllQuery()).get();
         assertHitCount(resp, DOC_COUNT);
     }

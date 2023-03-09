6ad71e6af96e5523d9962f0c733548651cd7438a
Suraj Singh <surajrider@gmail.com>
2023-03-08 21:56:29 -0800

    [Segment Replication] Add logs & force enable segrep
    Signed-off-by: Suraj Singh <surajrider@gmail.com>

--- server/src/main/java/org/opensearch/indices/replication/SegmentReplicationTargetService.java
+++ server/src/main/java/org/opensearch/indices/replication/SegmentReplicationTargetService.java
@@ -364,6 +364,7 @@ public class SegmentReplicationTargetService implements IndexEventListener {
                         }
                     } else {
                         onGoingReplications.fail(replicationId, new ReplicationFailedException("Segment Replication failed", e), false);
+                        logger.info("--> engine type {}", target.indexShard().getEngine().getClass());
                     }
                 }
             });
